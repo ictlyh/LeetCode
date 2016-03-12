75ee681d939702fbaec0fad6c772c2e70811133b
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-12 19:39:10 +0800

    Add 18, 209, 212, 217

---
+++ 209.cc
@@ -0,0 +1,24 @@
+﻿class Solution {
+public:
+    bool sumsOver(int s, vector<int> nums, int num) {
+        int res;
+        for (int i = 0; i <= nums.size() - num; i++) {
+            res = 0;
+            for (int j = 0; j < num; j++) {
+                res += nums[i + j];
+                if (res >=s ) return true;
+            }
+        }
+        return false;
+    }
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        for (int i = nums.size(); i > 0; i--) {
+            if (!sumsOver(s, nums, i)) {
+                if (i == nums.size()) return 0;
+                else return i + 1;
+            }
+        }
+        return 1;
+    }
+};
