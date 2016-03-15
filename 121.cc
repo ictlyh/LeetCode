b00ab8b876c7b3c654f4fb9186268c62a3c03727
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-15 23:15:40 +0800

    Add 62 80 121

---
+++ 121.cc
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) return 0;
+        vector<int> back(prices);
+        int pro = 0;
+        for (int i = prices.size() - 2; i >= 0; i--)
+            back[i] = prices[i] < back[i + 1] ? back[i + 1] : prices[i];
+        for (int i = 0; i < prices.size(); i++)
+            pro = pro < back[i] - prices[i] ? back[i] - prices[i] : pro;
+        return pro;
+    }
+};
---
+++ 80.cc
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        vector<int>::iterator ite = nums.begin();
+        int length = nums.size();
+        int val, count;
+        while (ite != nums.end()) {
+            val = *ite;
+            count = 1;
+            while( ++ite != nums.end() && *ite == val) {
+                count++;
+                if (count == 3) {
+                    ite = nums.erase(ite);
+                    length--;
+                    ite--;
+                    count = 2;
+                }
+            }
+        }
+        return length;
+    }
+};
