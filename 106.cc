53db10c3ca88d682f74f4c5a3a7832055570313d
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-17 23:41:05 +0800

    Add 63, 106, 268

---
+++ 106.cc
@@ -0,0 +1,74 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* build(vector<int>& inorder, int inbegin, int inend,
+        vector<int>& postorder, int postbegin, int postend) {
+            TreeNode* root;
+        if (inend - inbegin == 0)
+            root = NULL;
+        else if (inend - inbegin == 1)
+            root= new TreeNode(inorder[inbegin]);
+        else {
+            root = new TreeNode(postorder[--postend]);
+            int inidx = inbegin;
+            int postidx = postbegin;
+            while (inorder[inidx] != postorder[postend]) {
+                inidx++;
+                postidx++;
+            }
+            root->left = build(inorder, inbegin, inidx, postorder, postbegin, postidx);
+            root->right = build(inorder, inidx + 1, inend, postorder, postidx, postend);
+        }
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return build(inorder, 0, inorder.size(), postorder, 0, postorder.size());
+    }
+};
+
+// Memory Limit Exceeded
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        TreeNode* root;
+        if (inorder.size() == 0)
+            root = NULL;
+        else if (inorder.size() == 1)
+            root= new TreeNode(inorder.front());
+        else {
+            vector<int>::iterator postend = postorder.end();
+            postend--;
+            TreeNode* root = new TreeNode(*postend);
+            vector<int>::iterator inite = inorder.begin();
+            vector<int>::iterator postite = postorder.begin();
+            while (*inite != *postend) {
+                inite++;
+                postite++;
+            }
+            vector<int> lchildInorder(inorder.begin(), inite);
+            vector<int> rchildInorder(++inite, inorder.end());
+            vector<int> lchildPostorder(postorder.begin(), postite);
+            vector<int> rchildPostorder(postite, postend);
+            root->left = buildTree(lchildInorder, lchildPostorder);
+            root->right = buildTree(rchildInorder, rchildPostorder);
+        }
+        return root;
+    }
+};
---
+++ 63.cc
@@ -0,0 +1,32 @@
+﻿class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        int matrix[m][n];
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 0)
+                matrix[i][0] = 1;
+            else {
+                for (; i < m; i++)
+                    matrix[i][0] = 0;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 0)
+                matrix[0][i] = 1;
+            else {
+                for (; i < n; i++)
+                    matrix[0][i] = 0;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1)
+                    matrix[i][j] = 0;
+                else matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+            }
+        }     
+        return matrix[m - 1][n - 1];
+    }
+};
