085234534b7326655d2540c26e4455797c9db375
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-18 23:13:37 +0800

    Add 64, 105 Update 119

---
+++ 105.cc
@@ -0,0 +1,35 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* build(vector<int>& preorder, int prebegin, int preend,
+        vector<int>& inorder, int inbegin, int inend) {
+        TreeNode* root;
+        if (preend - prebegin == 0)
+            root = NULL;
+        else if (preend - prebegin == 1)
+            root= new TreeNode(preorder[prebegin]);
+        else {
+            root = new TreeNode(preorder[prebegin]);
+            int preidx = prebegin + 1;
+            int inidx = inbegin;
+            while (inorder[inidx] != preorder[prebegin]) {
+                preidx++;
+                inidx++;
+            }
+            root->left = build(preorder, prebegin + 1, preidx, inorder, inbegin, inidx);
+            root->right = build(preorder, preidx, preend, inorder, inidx + 1, inend);
+        }
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return build(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+    }
+};
--- 119.cc
+++ 119.cc
@@ -1,5 +1,23 @@
 ﻿class Solution {
 public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> matrix;
+        for (int i = 0; i <=rowIndex; i++) {
+            vector<int> row;
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i)
+                    row.push_back(1);
+                else row.push_back(matrix[i - 1][j - 1] + matrix[i - 1][j]);
+            }
+            matrix.push_back(row);
+        }
+        return matrix[rowIndex];
+    }
+};
+
+// Time Limit Exceeded
+class Solution {
+public:
     int getValue(int row, int col) {
         if (col == 0 || row == col) return 1;
         else return getValue(row - 1, col - 1) + getValue(row - 1, col);
