10b7330a48ce65ece6e665d8651bd8e026a5c044
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-19 22:57:55 +0800

    Add 53, 120, 152

---
+++ 152.cc
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int res = nums[0];
+        int prevMin = nums[0], prevMax = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            int prod1 = prevMin * nums[i];
+            int prod2 = prevMax * nums[i];
+            prevMin = min(nums[i], min(prod1, prod2));
+            prevMax = max(nums[i], max(prod1, prod2));
+            res = max(prevMax, res);
+        }
+        return res;
+    }
+};
---
+++ 53.cc
@@ -0,0 +1,11 @@
+﻿class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0], highest = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            sum = sum < 0 ? nums[i] : sum + nums[i];
+            highest = max(highest, sum);
+        }
+        return highest;
+    }
+};
