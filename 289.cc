635d138e5bfa4b42bdb833ccb1bf91a79b3bc2d1
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-03-23 23:13:35 +0800

    Add 54, 59, 289

---
+++ 289.cc
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+    bool nextState(vector<vector<int>>& board, int m, int n) {
+       int live_nei = 0;
+       for (int i = m - 1; i <= m + 1; i++) {
+           if (i < 0 || i >= board.size()) continue;
+           for (int j = n - 1; j <= n + 1; j++) {
+               if (j < 0 || j >= board[0].size()) continue;
+               live_nei += board[i][j];
+           }
+       }
+       live_nei -= board[m][n];
+       if (board[m][n] == 1) {
+           if (live_nei < 2 || live_nei > 3) return false;
+           else return true;
+       } else {
+           if (live_nei == 3) return true;
+           else return false;
+       }
+    }
+    void gameOfLife(vector<vector<int>>& board) {
+        vector<vector<int>> backup(board);
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (nextState(backup, i, j)) board[i][j] = 1;
+                else board[i][j] = 0;
+            }
+        }
+    }
+};
---
+++ 59.cc
@@ -0,0 +1,55 @@
+﻿class Solution {
+public:
+    void generate(vector<vector<int>>& res, int n, int val, int i, int j, int direc) {
+        res[i][j] = val;
+        if (val == n * n) return;
+        val++;
+        switch(direc) {
+            case 0:
+                if (j + 1 == n || res[i][j + 1]) {
+                    direc = 1;
+                    return generate(res, n, val, i + 1, j, direc);
+                } else {
+                    return generate(res, n, val, i, j + 1, direc);
+                }
+                break;
+            case 1:
+                if (i + 1 == n || res[i + 1][j]) {
+                    direc = 2;
+                    return generate(res, n, val, i, j - 1, direc);
+                } else {
+                    return generate(res, n, val, i + 1, j, direc);
+                }
+                break;
+            case 2:
+                if (j - 1 < 0 || res[i][j - 1]) {
+                    direc = 3;
+                    return generate(res, n, val, i - 1, j, direc);
+                } else {
+                    return generate(res, n, val, i, j - 1, direc);
+                }
+                break;
+            case 3:
+                if (i - 1 < 0 || res[i - 1][j]) {
+                    direc = 0;
+                    return generate(res, n, val, i, j + 1, direc);
+                } else {
+                    return generate(res, n, val, i - 1, j, direc);
+                }
+                break;
+            default: break;
+        }
+    }
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res;
+        if (n == 0) return res;
+        for (int i = 0; i < n; i++) {
+            vector<int> tmp;
+            for (int j = 0; j < n; j++)
+                tmp.push_back(0);
+            res.push_back(tmp);
+        }
+        generate(res, n, 1, 0, 0, 0);
+        return res;
+    }
+};
