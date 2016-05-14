004e0c50bc1baaf9aed47f7008cd781cf36c79d9
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-05-15 00:26:13 +0800

    Update 70

--- 70.cc
+++ 70.cc
@@ -4,6 +4,24 @@ class Solution {
       if (n == 0) return 0;
       if (n == 1) return 1;
       if (n == 2) return 2;
+      int res[2] = {1, 2};
+      int idx = 1;
+      int dir = -1;
+      for (int i = 2; i < n; i++) {
+        idx += dir;
+        res[idx] = res[0] + res[1];
+        dir = -dir;
+      }
+      return res[0] > res[1] ? res[0] : res[1];
+    }
+};
+
+class Solution {
+  public:
+    int climbStairs(int n) {
+      if (n == 0) return 0;
+      if (n == 1) return 1;
+      if (n == 2) return 2;
       vector<int> res;
       res.push_back(0);
       res.push_back(1);
