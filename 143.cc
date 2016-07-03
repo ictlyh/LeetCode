f25fdde1cf0f0b0d0cc9d23f91c4d3cf3f008543
ictlyh <luoyuanhao@software.ict.ac.cn>
2016-07-03 22:06:26 +0800

    Update

--- 143.cc
+++ 143.cc
@@ -7,41 +7,88 @@
  * };
  */
 class Solution {
-	public:
-		void insert(ListNode* head, ListNode* node, int n) {
-			ListNode* cur = head;
-			for (int i = 1; i < n - 1; i++) {
-				if (!cur->next) {
-					cur->next = node;
-					return;
-				} else {
-					cur = cur->next;
-				}
-			}
-			node->next = cur->next;
-			cur->next = node;
-		}
+  public:
+    ListNode* reverseList(ListNode* head) {
+      ListNode* dummy = new ListNode(-1);
+      while (head) {
+        ListNode* tmp = head->next;
+        head->next = dummy->next;
+        dummy->next = head;
+        head = tmp;
+      }
+      return dummy->next;
+    }
 
-		void reorderList(ListNode* head) {
-			if (!head || !head->next || !head->next->next) return ;
-			ListNode* slow = head;
-			ListNode* fast = head->next;
-			int size = 0;
-			while (fast && fast->next) {
-				slow = slow->next;
-				fast = fast->next->next;
-				size += 2;
-			}
-			if (!fast) size++;
-			else size += 2;
-			ListNode* cur = slow->next;
-			slow->next = NULL;
-			int idx = size / 2 + 1;
-			while (cur) {
-				ListNode* tmp = cur->next;
-				insert(head, cur, idx);
-				cur = tmp;
-				idx--;
-			}
-		}
+    void reorderList(ListNode* head) {
+      if (!head || !head->next || !head->next->next) return ;
+      ListNode* slow = head;
+      ListNode* fast = head->next->next;
+      while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+      }
+      ListNode* right = NULL;
+      if (fast) {
+        right = reverseList(slow->next->next);
+        slow->next->next = NULL;
+      } else {
+        right = reverseList(slow->next);
+        slow->next = NULL;
+      }
+      ListNode* ptr = head;
+      while (right) {
+        ListNode* tmp = right->next;
+        right->next = ptr->next;
+        ptr->next = right;
+        ptr = ptr->next->next;
+        right = tmp;
+      }
+    }
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    void insert(ListNode* head, ListNode* node, int n) {
+      ListNode* cur = head;
+      for (int i = 1; i < n - 1; i++) {
+        if (!cur->next) {
+          cur->next = node;
+          return;
+        } else {
+          cur = cur->next;
+        }
+      }
+      node->next = cur->next;
+      cur->next = node;
+    }
+
+    void reorderList(ListNode* head) {
+      if (!head || !head->next || !head->next->next) return ;
+      ListNode* slow = head;
+      ListNode* fast = head->next;
+      int size = 0;
+      while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        size += 2;
+      }
+      if (!fast) size++;
+      else size += 2;
+      ListNode* cur = slow->next;
+      slow->next = NULL;
+      int idx = size / 2 + 1;
+      while (cur) {
+        ListNode* tmp = cur->next;
+        insert(head, cur, idx);
+        cur = tmp;
+        idx--;
+      }
+    }
 };
--- 25.cc
+++ 25.cc
@@ -7,43 +7,90 @@
  * };
  */
 class Solution {
-	public:
-		ListNode* revertList(ListNode* head, ListNode** newHead) {
-			if (!head) return head;
-			if (head->next) {
-				ListNode* tail = revertList(head->next, newHead);
-				tail->next = head;
-				head->next = NULL;
-			}
-			else *newHead = head;
-			return head;
-		}
+  public:
+    ListNode* reverseList(ListNode* head) {
+      ListNode* dummy = new ListNode(-1);
+      while (head) {
+        ListNode* tmp = head->next;
+        head->next = dummy->next;
+        dummy->next = head;
+        head = tmp;
+      }
+      return dummy->next;
+    }
 
-		ListNode* reverseKGroup(ListNode* head, int k) {
-			if (!head || !head->next) return head;
-			ListNode* dummy = new ListNode(0);
-			dummy->next = head;
-			ListNode* tail = dummy;
-			ListNode* newHead = new ListNode(0);
-			ListNode* cur = head;
-			while (true) {
-				for (int i = 1; i < k; i++) {
-					if (cur) cur = cur->next;
-					else break;
-				}
-				if (cur) {
-					ListNode* tmp = cur->next;
-					cur->next = NULL;
-					ListNode* tmpTail = revertList(head, &newHead);
-					tail->next = newHead;
-					tail = tmpTail;
-					head = tmp;
-					cur = tmp;
-				} else {
-					tail->next = head;
-					break;
-				}
-			}
-			return dummy->next;
-		}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+      if (!head || !head->next) return head;
+      ListNode* dummy = new ListNode(0);
+      dummy->next = head;
+      ListNode* ptr = dummy;
+      ListNode* cur = head;
+      while (true) {
+        for (int i = 1; i < k; i++) {
+          if (cur) cur = cur->next;
+          else break;
+        }
+        if (cur) {
+          ListNode* tmp = cur->next;
+          cur->next = NULL;
+          ptr->next = reverseList(head);
+          ptr = head;
+          head = tmp;
+          cur = tmp;
+        } else {
+          ptr->next = head;
+          break;
+        }
+      }
+      return dummy->next;
+    }
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode* revertList(ListNode* head, ListNode** newHead) {
+      if (!head) return head;
+      if (head->next) {
+        ListNode* tail = revertList(head->next, newHead);
+        tail->next = head;
+        head->next = NULL;
+      }
+      else *newHead = head;
+      return head;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+      if (!head || !head->next) return head;
+      ListNode* dummy = new ListNode(0);
+      dummy->next = head;
+      ListNode* tail = dummy;
+      ListNode* newHead = new ListNode(0);
+      ListNode* cur = head;
+      while (true) {
+        for (int i = 1; i < k; i++) {
+          if (cur) cur = cur->next;
+          else break;
+        }
+        if (cur) {
+          ListNode* tmp = cur->next;
+          cur->next = NULL;
+          ListNode* tmpTail = revertList(head, &newHead);
+          tail->next = newHead;
+          tail = tmpTail;
+          head = tmp;
+          cur = tmp;
+        } else {
+          tail->next = head;
+          break;
+        }
+      }
+      return dummy->next;
+    }
 };
